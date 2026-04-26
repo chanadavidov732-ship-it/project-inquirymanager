672a5b73f62adae2eadc4f381c3b559ea10f5c75
Elisheva <e0583292053@gmail.com>
2026-04-26 16:55:53 +0300

    feat: implemented ADD_INQUIRY logic with persistent ID assignment

--- IntelliJTraining_inquirymanager_chanadavidov/src/Server/ClientHandler.java
+++ IntelliJTraining_inquirymanager_chanadavidov/src/Server/ClientHandler.java
@@ -1,15 +1,17 @@
 package Server;
 
-import Shared.RequestObj;
-import Shared.ResponseObj;
+import Shared.*;
+import HandleStoreFiles.HandleFiles;
 import java.io.*;
 import java.net.Socket;
 
 public class ClientHandler implements Runnable {
     private Socket socket;
+    private HandleFiles fileHandler;
 
     public ClientHandler(Socket socket) {
         this.socket = socket;
+        this.fileHandler = new HandleFiles();
     }
 
     @Override
@@ -18,26 +20,34 @@ public class ClientHandler implements Runnable {
              ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream())) {
 
             RequestObj request = (RequestObj) input.readObject();
-            System.out.println("Received request: " + request.getAction());
-
             ResponseObj response;
-            if (request.getAction() == RequestObj.Action.TEST) {
-                response = new ResponseObj(200, "Success", "SERVER_READY");
-            } else {
-                response = new ResponseObj(400, "Action not implemented yet", null);
+
+            switch (request.getAction()) {
+                case TEST:
+                    response = new ResponseObj(200, "Success", "SERVER_READY");
+                    break;
+
+                case ADD_INQUIRY:
+                    Inquiry newInquiry = (Inquiry) request.getParams();
+
+                    int nextId = fileHandler.getNextIdAndIncrement();
+
+                    newInquiry.setCode(nextId);
+
+                    fileHandler.saveFile(newInquiry);
+
+                    response = new ResponseObj(200, "Inquiry added successfully", nextId);
+                    break;
+
+                default:
+                    response = new ResponseObj(400, "Unknown Action", null);
             }
 
             output.writeObject(response);
             output.flush();
 
-        } catch (IOException | ClassNotFoundException e) {
-            System.err.println("Handler exception: " + e.getMessage());
-        } finally {
-            try {
-                socket.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        } catch (Exception e) {
+            System.err.println("Error handling client: " + e.getMessage());
         }
     }
 }
